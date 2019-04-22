f78d383009184d3be3d0f694f530fdd9a0ef261f
Carlos Eduardo <carloseduardo.oceano@gmail.com>
2019-04-22 10:06:04 -0300

    V1.0.1

--- test/index.php
+++ test/index.php
@@ -3,10 +3,9 @@ require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email(2, 'mail.criativawebdesign.com.br', 'contato@criativawebdesign.com.br', 'MegC2004',
-    'tls', '587', 'carloseduardo.oceano@gmail.com', 'Carlos - Curso Composer');
-$novoEmail->sendMail("Teste curso Composer", "<p>Este é um e-mail de teste do curso <b>Composer da UpInside</b></p>",
-    "carloseduardo.oceano@gmail.com", "Carlos Eduardo",
-    "contato@criativawebdesign.com.br", "Carlos Criativa Web");
+$email = new Email(2, "mail.host.com", "your@email.com", "your-pass", "smtp secure (tls/ssl)", "port (587)",
+    "from@email.com", "From Name");
+
+$email->sendEmail("Subject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
 
 //var_dump($novoEmail);
